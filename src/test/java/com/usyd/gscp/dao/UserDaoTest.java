d1153659a6c9536d63c7955ab99144e593bdeb51
Haifeng Zhao <hzha7521@uni.sydney.edu.au>
2017-10-23 00:10:01 +1100

    Update pom.xml to make the test run.

---
+++ src/test/java/com/usyd/gscp/dao/UserDaoTest.java
@@ -0,0 +1,33 @@
+package com.usyd.gscp.dao;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/appServlet/hibernate-context.xml"})
+public class UserDaoTest{
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Test
+    @Transactional
+    @Rollback(true)
+	public void testGetAllUsers() {
+		List users = userDao.getAllUsers();
+		Assert.assertFalse(users.isEmpty());
+	}
+	
+}
